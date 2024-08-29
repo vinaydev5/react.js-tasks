8c801052db757eb13b6d1302528ea2bac1769070
vinaydev5 <doddavarapuvinaybabu@gmail.com>
2024-08-29 17:13:35 +0530

    tasks of react.js

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import ComponentName from "./bulb"
+
+// function App(){
+//    return(
+//     <Card/>
+
+//    );
+
+
+
+// }
+
+
+
+
+
+const App =() =>{
+   return(
+    <div>
+    <ComponentName/>
+    </div>
+   )
+
+
+}
+
+export default App
+
+
+
